30aede9444971365bf847754de743a00fb1cc9f6
Ebuka <ebukaobiora10@gmail.com>
2023-02-06 21:31:15 +0100

    authentication

--- dea-api/controllers/post.js
+++ dea-api/controllers/post.js
@@ -11,14 +11,9 @@ exports.getPosts = (req, res) => {
 exports.createPost = (req, res) =>{
     const post = new Post(req.body);
     //console.log("CREATING POST ", post)
-    post.save((err, result)=>{
-        if(err){
-            return res.status(400).json({
-                error: err
-            })
-        }
+    post.save().then(result => {
         res.status(200).json({
             post: result
-        })
-    })
+        });
+    });
 };
