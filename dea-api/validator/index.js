7c2b71742b0fd93dba9c1edb784bc0d0a1acd5ce
Ebuka <ebukaobiora10@gmail.com>
2023-02-16 00:15:24 +0100

    user signin validation + error message

--- dea-api/validator/index.js
+++ dea-api/validator/index.js
@@ -22,3 +22,32 @@ exports.createPostValidator = (req, res, next) =>{
     //proceed to next middleware 
     next();
 };
+
+exports.userSignupValidator = (req, res, next) => {
+    // name is not null and is between 4-10 characters
+    req.check("name","Name is required").notEmpty();
+    //email is not null, valid and normalized
+    req.check("email", "Email must be between 3 to 32 characters")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @")
+    .isLength({
+        min: 4,
+        max: 200
+    })
+    //check for password
+    req.check("password", "Password is required").notEmpty();
+    req.check('password')
+    .isLength({min:6})
+    .withMessage("Password must contain at least 6 characters")
+    .matches(/\d/)
+    .withMessage("Password must contain a number")
+    //check for errors
+    const errors = req.validationErrors()
+    //if error shows the first as it happens
+    if(errors) {
+        const firstError = errors.map(() => error.msg)[0]
+        return res.status(400).json({error: firstError})
+    }
+    //proceed to next middleware 
+    next();
+}
